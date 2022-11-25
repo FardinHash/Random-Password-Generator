05f1f065ac626a6f473695cf62a8f0b83e8e1623
Fardin <60337534+FardinHash@users.noreply.github.com>
2022-11-25 20:56:19 +0600

    Using Random module

--- randompass_gen.py
+++ randompass_gen.py
@@ -1,12 +1,19 @@
+#import library
 import random
 
+#define combinations
 lowers = 'abcdefghijklmnopqrstuvwxyz'
 uppers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 numbers = '012345678'
 symbols = '!@#$%^&*'
 
 combination = lowers + uppers + numbers + symbols
+
+#fix the length of password
 length = 8
+
+#generating the password
 password = ''.join(random.sample(combination, length))
 
-print(password)
+#here the password you got!
+print(password) 
